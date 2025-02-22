b87635f826972ea12e16eb94a9cd72d120d02239
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-02-22 00:58:41 +0000

    add middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.asset import Asset
 from app.core.portfolio import Portfolio
 
@@ -6,6 +7,14 @@ from app.routers.asset import router as asset_router
 
 app = FastAPI()
 app.include_router(asset_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get("/")
 def read_root():
