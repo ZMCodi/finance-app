7e29a1cc173352f3379645e3a480db38a654d6b2
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-03-05 09:58:27 +0000

    wrap searchparams in suspenese

--- frontend/app/assets/details/page.tsx
+++ frontend/app/assets/details/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 import TickerPageClient from '@/app/assets/details/components/TickerPageClient';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function TickerPage() {
     const searchParams = useSearchParams();
     const ticker = searchParams.get('ticker');
-    return <TickerPageClient ticker={ticker} />;
+    
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <TickerPageClient ticker={ticker} />
+        </Suspense>
+    )
 }
